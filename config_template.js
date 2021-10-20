b3c33a8f60f2d3618e1764b7f3bdf7954b251708
Supa <36031171+0Supa@users.noreply.github.com>
2021-10-20 17:52:35 +0300

    Initial commit

---
+++ config_template.js
@@ -0,0 +1,5 @@
+module.exports = {
+    "discordWebhookURL": "https://discord.com/api/webhooks/xxxxxx/xxxxxx",
+    "channels": ["forsen"],
+    "regexTriggers": [/\b8supa\b/i, /\bxd\b/]
+}
---
+++ index.js
@@ -0,0 +1,63 @@
+const config = require('./config.js');
+const { ChatClient } = require("dank-twitch-irc");
+const got = require('got');
+const triggersLength = config.regexTriggers.length
+const cooldown = new Set();
+
+const client = new ChatClient();
+
+async function send(embeds) {
+    try {
+        await got.post(config.discordWebhookURL, {
+            json: { embeds }
+        });
+    } catch (err) {
+        console.error(`couldn't send mention to the Discord Webhook: ${err.message}`)
+    }
+}
+
+client.on("ready", () => {
+    console.log('Chat connected')
+    client.joinAll(config.channels)
+});
+
+client.on("JOIN", ({ channelName }) => {
+    console.log(`Joined ${channelName}`)
+});
+
+client.on("PART", ({ channelName }) => {
+    console.log(`Parted ${channelName}`)
+});
+
+client.on("PRIVMSG", async (msg) => {
+    for (let i = 0; i < triggersLength; i++) {
+        if (!cooldown.has(msg.senderUserID) && config.regexTriggers[i].test(msg.messageText)) {
+            cooldown.add(msg.senderUserID);
+            setTimeout(() => {
+                cooldown.delete(msg.senderUserID);
+            }, 1500);
+
+            await send([{
+                "color": 16776960,
+                "fields": [
+                    {
+                        "name": "User",
+                        "value": msg.senderUsername,
+                        "inline": true
+                    },
+                    {
+                        "name": "Channel",
+                        "value": msg.channelName,
+                        "inline": true
+                    },
+                    {
+                        "name": "Message",
+                        "value": msg.messageText
+                    }
+                ]
+            }])
+        }
+    }
+});
+
+client.connect()
