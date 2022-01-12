0ee72b7aef35bfd1de933a18f13c164aa683ce38
Supa <36031171+0Supa@users.noreply.github.com>
2022-01-12 18:37:59 +0200

    Update index.js

--- index.js
+++ index.js
@@ -59,7 +59,7 @@ client.on("PRIVMSG", async (msg) => {
                     },
                     {
                         "name": "Message",
-                        "value": msg.messageText.replace(/([`*~>|_])/g, '\\$1')
+                        "value": msg.messageText.replace(/([`*~>|_])/g, `$1${String.fromCharCode(8203)}`)
                     }
                 ]
             }])
