0cd110f40d1d47ee6f8580752ff61904bdaff79d
francorz <francorz2004@gmail.com>
2024-06-02 19:19:05 -0500

    forsen

--- config_template.js
+++ config_template.js
@@ -1,10 +1,23 @@
 module.exports = {
-    "discordWebhookURL": "url",
-    "ignoredUsers": ["user", "user"],
+    "webhookURLs": {
+        "all": "https://discord.com/api/webhooks/link",
+        "alert": "https://discord.com/api/webhooks/link"
+    },
+    "ignoredUsers": [
+        "user",
+        "user"
+    ],
     "auth": {
         "Authorization": "Bearer token",
         "Client-Id": "id"
     },
-    "channels": ["channel", "channel"],
+    "channels1": [
+        "channel",
+        "channel"
+    ],
+    "channels2": [
+        "channel",
+        "channel"
+    ],
     "regexTriggers": [/regex/i]
 }
